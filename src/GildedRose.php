1ce9cabe299e8bca3b59c577d9611d1bbcd794b1
EnriqueSaenz <enriquesaenz0@gmail.com>
2026-01-20 23:57:06 -0500

    add validations of new product

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -16,51 +16,82 @@ final class GildedRose
 
     public function updateQuality(): void
     {
+        $AGED_BRIE = 'Aged Brie';
+        $BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert';
+        $SULFURAS = 'Sulfuras, Hand of Ragnaros';
+        $CONJURED = 'Conjured Mana Cake';
+
+        $MAX_QUALITY = 50;
+
         foreach ($this->items as $item) {
-            if ($item->name !== 'Aged Brie' and $item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            switch ($item->name) {
+                case $AGED_BRIE:
 
-            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                $item->sellIn = $item->sellIn - 1;
-            }
+                    $incrementIn = $item->sellIn > 0 ? 1 : 2;
 
-            if ($item->sellIn < 0) {
-                if ($item->name !== 'Aged Brie') {
-                    if ($item->name !== 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name !== 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
+                    if ($item->quality + $incrementIn >= $MAX_QUALITY) {
+                        $item->quality = $MAX_QUALITY;
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->quality += $incrementIn;
                     }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+
+                    $item->sellIn--;
+
+                    break;
+                case $BACKSTAGE:
+                    $incrementIn = 0;
+                    switch (true) {
+                        case $item->sellIn <= 0:
+                            $incrementIn = 0;
+                            break;
+                        case $item->sellIn <= 5:
+                            $incrementIn += 3;
+                            break;
+                        case $item->sellIn <= 10:
+                            $incrementIn += 2;
+                            break;
+                        default:
+                            $incrementIn++;
+                            break;
                     }
-                }
+
+                    if ($incrementIn === 0) {
+                        $item->quality = 0;
+                    } else {
+                        $item->quality = $item->quality + $incrementIn <= $MAX_QUALITY ?
+                            $item->quality + $incrementIn :
+                            $MAX_QUALITY;
+                    }
+
+                    $item->sellIn--;
+
+                    break;
+                case $SULFURAS:
+                    break;
+                case $CONJURED:
+                    $decrementIn = $item->sellIn >= 0 ? 2 : 4;
+
+                    if ($item->quality - $decrementIn <= 0) {
+                        $item->quality = 0;
+                    } else {
+                        $item->quality -= $decrementIn;
+                    }
+
+                    $item->sellIn--;
+                    break;
+                default:
+
+                    $decrementIn = $item->sellIn > 0 ? 1 : 2;
+
+                    if ($item->quality - $decrementIn <= 0) {
+                        $item->quality = 0;
+                    } else {
+                        $item->quality -= $decrementIn;
+                    }
+
+                    $item->sellIn--;
+
+                    break;
             }
         }
     }
